9e4f54946b8ca396aa764a0754e1437de649a287
Ann Kariuki <awk19@duke.edu>
2026-02-02 21:07:51 -0800

    Create class version of Cart

---
+++ data/cart-class.js
@@ -0,0 +1,113 @@
+class Cart {
+    cartItems;
+    localStorageKey;
+
+    constructor(localStorageKey) {
+        this.localStorageKey = localStorageKey;
+        this.loadFromStorage();
+    }
+
+    loadFromStorage() {
+        this.cartItems = JSON.parse(localStorage.getItem(this.localStorageKey));
+        //getItem returns the string version of the array. We need to convert back to array using JSON.parse
+
+        // if we dont have a cart in storage cart === NULL so NOT NULL (!cart) becomes truthy, so set the default cart
+        if (!this.cartItems) {
+            this.cartItems = [{
+                productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+                quantity: 2,
+                deliveryOptionsId: '1'
+            },
+            {
+                productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+                quantity: 1,
+                deliveryOptionsId: '2'
+            }];
+        }
+    }
+
+    saveToStorage() {
+        //local storage only saves strings
+        localStorage.setItem(this.localStorageKey, JSON.stringify(this.cartItems));
+    }
+
+    addToCart(productId) {
+        let matchingItem;
+        this.cartItems.forEach((cartItem) => {
+            if (productId === cartItem.productId) {
+                matchingItem = cartItem;
+            }
+        });
+
+        if (matchingItem) {
+            matchingItem.quantity += 1;
+        } else {
+            this.cartItems.push({
+                productId: productId,
+                quantity: 1,
+                deliveryOptionsId: '1'
+            });
+        }
+
+        this.saveToStorage();
+    }
+
+    removeFromCart(productId) {
+        const newCart = [];
+        this.cartItems.forEach((cartItem) => {
+            if (cartItem.productId !== productId) {
+                newCart.push(cartItem);
+            }
+        });
+
+        this.cartItems = newCart;
+
+        this.saveToStorage();
+    }
+
+    updateQuantity(productId, newQuantity) {
+        let matchingItem;
+
+        this.cartItems.forEach((cartItem) => {
+            if (productId === cartItem.productId) {
+                matchingItem = cartItem;
+            }
+        });
+
+        matchingItem.quantity = newQuantity;
+
+        this.saveToStorage();
+    }
+
+    updateDeliveryOption(productId, deliveryOptionId) {
+        let matchingItem;
+
+        this.cartItems.forEach((cartItem) => {
+            if (productId === cartItem.productId) {
+                matchingItem = cartItem;
+            }
+        });
+
+        matchingItem.deliveryOptionsId = deliveryOptionId;
+
+        this.saveToStorage();
+    }
+
+    calculateCartQuantity() {
+        let cartQuantity = 0;
+
+        this.cartItems.forEach((cartItem) => {
+            cartQuantity += cartItem.quantity;
+        });
+
+        return cartQuantity;
+    }
+}
+
+
+const cart = new Cart('cart-oop');
+const businessCart = new Cart('cart-business');
+
+console.log(cart);
+console.log(businessCart);
+console.log(businessCart instanceof Cart);
